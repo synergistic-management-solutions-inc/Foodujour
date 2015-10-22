ec5fc47a9b5f9d95eaf0aec48b87e882ac2776fb
Adam Ziehl <ajziehl@gmail.com>
2015-10-22 12:20:49 -0500

    Fix formatting

--- client/controllers/about.js
+++ client/controllers/about.js
@@ -1,3 +1,3 @@
-app.controller('AboutCtrl', ['$scope','$http', function($scope,$http) {
+app.controller('AboutCtrl', ['$scope', '$http', function($scope,$http) {
   $scope.header = 'I am ready to be built!';
 }]);
