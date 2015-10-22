e04c8ee0a48998bbea7aed3a72882a2553039dad
Adam Ziehl <ajziehl@gmail.com>
2015-10-22 17:24:15 -0500

    Remove some console logs

--- server/lib/db.js
+++ server/lib/db.js
@@ -5,9 +5,6 @@ var config = require('../../knexfile.js');
 
 // Configure knex with the correct environment configuration
 var env = process.env.NODE_ENV || 'development';
-console.log('db line 9:', env);
-console.log('db line 8:', config[env]);
-console.log('db line 10:', process.env.PG_TEST);
 var db = require('knex')(config[env]);
 
 // Export the db object, which will be able to make database connections
