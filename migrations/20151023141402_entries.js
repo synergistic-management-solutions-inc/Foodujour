01ccb9246602115a65dc1f390ebc25d522111736
rsoule3 <robsoule3@gmail.com>
2015-10-23 17:36:33 -0500

    Upgraded Migrations to not allow null values for certain values

--- migrations/20151023141402_entries.js
+++ migrations/20151023141402_entries.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('entries', function (ent) {
     ent.increments('id').primary();
-    ent.integer('meal_id');
-    ent.string('name', 255);
+    ent.integer('meal_id').notNullable();
+    ent.string('name', 255).notNullable();
     ent.integer('rating');
     ent.string('notes', 255);
     ent.string('image', 255);
