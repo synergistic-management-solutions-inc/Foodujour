149bc22da1fcbb3129f1f4d1def7b3bee2c0fda1
sca <smartinsantos@gmail.com>
2015-10-23 07:40:34 -0500

    added table schemas

--- server/lib/db.js
+++ server/lib/db.js
@@ -20,3 +20,55 @@ db.deleteEverything = function () {
   // TODO: Delete data from all tables (useful for testing)
   return db('meals').delete();
 };
+
+
+//Table schema for Meals that will contain entries
+db.knex.schema.hasTable('meals').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('meals', function (meal) {
+      meal.increments('id').primary();
+      meal.string('user', 255);
+      meal.string('name', 255);
+      meal.string('location', 255);
+      meal.string('date')
+      meal.integer('rating');
+      meal.string('notes');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+// Table schema for entries 'sub-table of Meals'
+db.knex.schema.hasTable('entries').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('entries', function (ent) {
+      ent.increments('id').primary();
+      ent.string('name');
+      ent.string('notes');
+      ent.string('image');
+      ent.integer('meal_id');
+      ent.integer('rating');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+//Depends on Passport behavior 
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function(user) {
+//       user.increments('id').primary();
+//       user.string('username', 255);
+//       user.string('password', 255);
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+
+
+module.exports = db;
