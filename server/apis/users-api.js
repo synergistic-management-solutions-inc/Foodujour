8be6644027819381a654082170cc09fc3b26a9bc
rsoule3 <robsoule3@gmail.com>
2015-10-24 16:03:20 -0500

    Formatting fix on users api

--- server/apis/users-api.js
+++ server/apis/users-api.js
@@ -7,16 +7,21 @@ var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 var UsersAPI = express.Router();
 
 UsersAPI.get('/auth/google',
-  passport.authenticate('google', { scope: 
-    [ 'https://www.googleapis.com/auth/plus.login','https://www.googleapis.com/auth/userinfo.profile','https://www.googleapis.com/auth/userinfo.email',
-    , 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
-));
- 
-UsersAPI.get( '/auth/google/callback', 
-    passport.authenticate( 'google', { 
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
-}));
+  passport.authenticate('google', {
+    scope: [
+      'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/plus.profile.emails.read'
+    ]
+  })
+);
 
+UsersAPI.get( '/auth/google/callback',
+  passport.authenticate( 'google', {
+    successRedirect: '/auth/google/success',
+    failureRedirect: '/auth/google/failure'
+  })
+);
 
 module.exports = UsersAPI;
