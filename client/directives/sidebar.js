57db33a3d9e6db3dce18259e28afd6c1d540c003
Kevin Saldana <ksaldana1@yahoo.com>
2015-10-23 23:30:40 -0500

    added comment: sidebar directive

--- client/directives/sidebar.js
+++ client/directives/sidebar.js
@@ -1,5 +1,9 @@
+// sidebar directive component for re-use
 app.directive('sidebar', function() {
   return  {
+    // use as attribute or element
+    restrict: 'AE',
+    // use sidebar view as template
     templateUrl: '../views/sidebar.html'
   }
 });
