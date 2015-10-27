4c0a7d938476e8ed42c840e86ed49e6a2b38d3e1
James Hicks <jehicks2@yahoo.com>
2015-10-27 16:43:59 -0500

    Meals Populate page. Working on entries

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -4,11 +4,12 @@ app.controller('mealView', ['$scope', '$http', function($scope,$http) {
 
   $http.get("/meals")
    .then(function(data){
-   	console.log('Data: ', data.data)
+   	// console.log('Data: ', data.data)
    	for(var i = 0; i < data.data.length; i++){
    	// console.log('Name: ', data.data[i].name)
-   	$scope.meals = data.data;
     // console.log(data.data[i])
     }
+   	$scope.meals = data.data;
    })
 }]);
+
