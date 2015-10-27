e49d7ab177832acb6a07ba424153b9e2c4cc9818
James Hicks <jehicks2@yahoo.com>
2015-10-27 15:34:25 -0500

    Feed showing

--- client/controllers/entryView.js
+++ client/controllers/entryView.js
@@ -3,6 +3,7 @@ app.controller('entryView', ['$scope', '$http', function($scope,$http) {
 
   $http.get("/entries")
    .then(function(data){
+   	console.log('entry: ', data)
    	// console.log('Data: ', data)
    	// for(var i = 0; i < data.data.length; i++){
    	// console.log('Name: ', data.data[i].name)
