a986f3cb96dcb28985e2f83ad3eeb979f114b7f2
James Hicks <jehicks2@yahoo.com>
2015-10-28 09:07:42 -0500

    Merge

---
+++ client/controllers/entryForm.js
@@ -0,0 +1,25 @@
+// currently un-used. To be used for sign-in/sign-up
+app.controller('EntryForm', ['$scope', '$http', function($scope, $http) {
+  console.log('entryForm Controller');
+
+  $scope.stuff = [2,3,4];
+
+  $scope.entry = {};
+  $scope.entry.name = 'Pickles';
+  $scope.entry.rating = 10;
+  $scope.entry.notes = 'Tasted like cucumbers';
+  $scope.entry.image = '';
+
+  $scope.saveEntry = function() {
+    console.log('Save Entry');
+    // console.log('Meal name:', $scope.meal.name);
+    // console.log('Meal date:', $scope.meal.date);
+    // console.log('Meal location:', $scope.meal.location);
+    // console.log('Meal rating:', $scope.meal.rating);
+    // console.log('Meal notes:', $scope.meal.notes);
+    // console.log('Meal image:', $scope.meal.image);
+    // console.log('Meal entries:', $scope.meal.entries);
+
+    // MealForm.addMeal($scope.meal);
+  };
+}]);
