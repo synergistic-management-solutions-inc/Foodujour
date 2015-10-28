7719aec55930a9ec56d0c182c81cac54fcdd69f3
James Hicks <jehicks2@yahoo.com>
2015-10-28 18:56:57 -0500

    Entries

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -6,17 +6,25 @@ app.controller('mealView', ['$scope', '$http', function($scope,$http) {
    .then(function(data){
    	 // console.log('Data: ', data.data)
    	 for(var i = 0; i < data.data.length; i++){
+
    	 // console.log('Name: ', data.data[i].id)
+
+   	 console.log('Number: ', data.data[i].id)
+
    	 $scope.meals = data.data;
     // console.log(data.data[i])
     // }
 
     $http.get("/meals" + "/" + data.data[i].id)
      .then(function(data){
-    for(var i = 0; i< data.data.entries.length; i++){
-     	// console.log("Entry data", data.data.entries[i])
-    }
-    	// if(data.data.entries[i].)
+
+     	console.log("haha", data.data.entries.length)
+      for(var i = 0; i< data.data.entries.length; i++){
+     	// console.log("Entry data: ", data.data.entries[i])
+     	console.log("Meal_id: ",data.data.entries[i])
+    	// if(data.data.entries[i].meal_id === data.data[i].id){
+      }
+
         $scope.entries = data.data.entries;
      
      })
