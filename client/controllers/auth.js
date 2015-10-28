89a3b3efd914ef73c4f8a0681b9a0c6d259a8f25
sca <smartinsantos@gmail.com>
2015-10-28 14:27:17 -0500

    added functionality to sign-in form with angular routes

--- client/controllers/auth.js
+++ client/controllers/auth.js
@@ -1,4 +1,41 @@
 // currently un-used. To be used for sign-in/sign-up
-app.controller('AuthCtrl', ['$scope', function($scope,$http) {
-  $scope.auth = {};
+app.controller('AuthCtrl', ['$scope','$http', function($scope,$http) {
+  $scope.user = {
+    mail: '',
+    password: ''
+  };
+
+  $scope.logIn = function(){
+    console.log('submitting')
+
+    $http.post("/users/auth/login")
+    .then(function(data){
+    console.log('logged in ', data)
+    })
+    // .catch{
+    //   console.log('rejected mf')
+    // }
+  };
+
+  $scope.signUp = function(){
+
+    console.log('submitting')
+
+  };
+
+  $scope.google = function(){
+
+    console.log('submitting')
+
+  };
+
+    $scope.facebook = function(){
+
+    console.log('submitting')
+
+  };
+
+
+
+
 }]);
