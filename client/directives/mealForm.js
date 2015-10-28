0d5e01cc942ce0506c33d595d5b5a99ecc95f50f
James Hicks <jehicks2@yahoo.com>
2015-10-28 10:04:23 -0500

    Fixed MealForm bug

--- client/directives/mealForm.js
+++ client/directives/mealForm.js
@@ -1,7 +1,7 @@
 // meal user input form component for re-use
 // component will be accessed from collapisble on user home page
 // will have button to append entry forms
-app.directive('mealform', function() {
+app.directive('mealForm', function() {
   return  {
     restrict: 'E',
     // use meal form template for directive
