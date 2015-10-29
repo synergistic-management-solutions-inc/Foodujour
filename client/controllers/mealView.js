0e6b51b486ae530d1b5ec186531de7622cf42861
James Hicks <jehicks2@yahoo.com>
2015-10-28 19:24:03 -0500

    Playing with entries

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -6,52 +6,20 @@ app.controller('mealView', ['$scope', '$http', function($scope,$http) {
    .then(function(data){
    	 // console.log('Data: ', data.data)
    	 for(var i = 0; i < data.data.length; i++){
-
-
    	 console.log('Number: ', data.data[i].id)
-
    	 $scope.meals = data.data;
     // console.log(data.data[i])
     // }
-    $http.get("/meals" + "/" + $scope.meals[i].id)
-     .then(function(data){
-
-
-
-
-     	console.log("haha", data.data.entries.length)
-      for(var i = 0; i< data.data.entries.length; i++){
-     	// console.log("Entry data: ", data.data.entries[i])
-     	console.log("Meal_id: ",data.data.entries[i])
-    	// if(data.data.entries[i].meal_id === data.data[i].id){
-
     
-
-     // 	console.log("haha", data.data.entries.length)
-     //  for(var i = 0; i< data.data.entries.length; i++){
-     // 	// console.log("Entry data: ", data.data.entries[i])
-     // 	console.log("Meal_id: ",data.data.entries[i])
-    	// // if(data.data.entries[i].meal_id === data.data[i].id){
-     //  }
-
-
-
+   return $http.get("/meals" + "/" + data.data[i].id)
+     .then(function(data){
       for(var j = 0; j< data.data.entries.length; j++){
      	// console.log("Entry data: ", data.data.entries[j])
      	console.log("Meal_id: ",data.data.entries[j])
     	// if(data.data.entries[j].meal_id === data.data[j].id){
 
-
-
         $scope.entries = data.data.entries;
-
-     	// console.log("ID", $scope.meals.id)
-      for(var j = 0; j< data.data.entries.length; j++){
-     	// console.log("Meal_id: ",data.data.entries[j])
-
-      }
-        $scope.entries = data.data.entries;
-     
+     }
      })
  	}
    })
