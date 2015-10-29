d24394492718d422b898db642ee8c2b2757c7c43
James Hicks <jehicks2@yahoo.com>
2015-10-29 18:16:26 -0500

    mealView modal

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -1,30 +1,35 @@
 // to be replaced with another controller
-app.controller('mealView', ['$scope', '$http', '$location', function($scope, $http, $location) {
+app.controller('mealView', ['$scope', '$http', function($scope, $http) {
   
 
 
   $http.get('/api/meals')
   .then(function(data){
     console.log('Data: ', data.data)
-    // for(var i = 0; i < data.data.length; i++){
+    for(var i = 0; i < data.data.length; i++){
     // console.log('Number: ', data.data[i].id)
     $scope.meals = data.data;
 
     // }
     // $http.get("/meals" + "/" + $scope.meals[i].id)
-    //  .then(function(data){
+     // .then(function(data){
     //   for(var j = 0; j< data.data.entries.length; j++){
-    //  	// console.log("Entry data: ", data.data.entries[j])
-    //  	console.log("Meal_id: ",data.data.entries[j])
-    // 	// if(data.data.entries[j].meal_id === data.data[j].id){
-    //     $scope.entries = data.data.entries;
+    //    // console.log("Entry data: ", data.data.entries[j])
+    //    console.log("Meal_id: ",data.data.entries[j])
+    //  // if(data.data.entries[j].meal_id === data.data[j].id){
+        // $scope.entries = data.data.entries;
     //   }
-    //  })
+     // })
     // }
 
- 	// }
- 	 $scope.setMeal = function (data) {
-            $scope.meal = data;
-      };
+    }
+
    });
+
+  $scope.showModal = function(m) {
+    $scope.meal = m;
+    
+    console.log("this is m: ", m)
+  };
 }]);
+
