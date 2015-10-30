ad8772ef7515a17bd5bbbb35a49596a67e12085a
James Hicks <jehicks2@yahoo.com>
2015-10-29 20:09:54 -0500

    Meal and Entry Modal working, styling needed

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -6,21 +6,13 @@ app.controller('mealView', ['$scope', '$http', function($scope, $http) {
   $http.get('/api/meals')
   .then(function(data){
     console.log('Data: ', data.data)
-    // for(var i = 0; i < data.data.length; i++){
-    // console.log('Number: ', data.data[i].id)
     $scope.meals = data.data;
 
-    // }
     $http.get("/api/entries")
      .then(function(data){
       console.log("what is:", data.data)
-      // for(var j = 0; j< data.data.entries.length; j++){
-       // console.log("Entry data: ", data.data.entries)
-    //    console.log("Meal_id: ",data.data.entries[j])
-      // }
         $scope.entries = data.data;
      })
-    // }
    });
 
   
