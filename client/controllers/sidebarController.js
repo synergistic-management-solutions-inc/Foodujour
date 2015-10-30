597ffdf98a47f63087118271adce499259e571ef
Adam Ziehl <ajziehl@gmail.com>
2015-10-29 19:17:19 -0500

    Accidentally removed Auth provider from sidebar controller

--- client/controllers/sidebarController.js
+++ client/controllers/sidebarController.js
@@ -1,5 +1,5 @@
 // placeholder controller
-app.controller('SidebarController', ['$scope', function($scope) {
+app.controller('SidebarController', ['$scope', 'Auth', function($scope, Auth) {
 
   $scope.signOut = function() {
     $('#sidenav-overlay').trigger('click');
