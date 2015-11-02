799f9ea734ce62ee0f631db6a8d30976af6d2f67
rsoule3 <robsoule3@gmail.com>
2015-11-01 19:38:24 -0600

    Heroku progress

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,22 @@
 require('dotenv').load();
 
 module.exports = {
+  production: {
+    client: 'pg',
+    connection: process.env.PG_PROD,
+    ssl: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
 
   development: {
     client: 'pg',
