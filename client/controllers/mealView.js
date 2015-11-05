4cbe6785db17ce08373981080df39bf8fd7510cd
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-04 18:36:44 -0600

    date bug fixed

--- client/controllers/mealView.js
+++ client/controllers/mealView.js
@@ -28,7 +28,7 @@ app.controller('MealView', ['$scope', '$http', '$state', 'MealForm', 'ConvertDat
   $scope.openForm = function(m){    
     if (m){
       MealForm.meal.id = m.id;
-      m.date = ConvertDate.convert(m.date);
+      var date = ConvertDate.convert(m.date);
     }
 
     //if no meal was provided, newMeal should be set to true
@@ -39,8 +39,7 @@ app.controller('MealView', ['$scope', '$http', '$state', 'MealForm', 'ConvertDat
     MealForm.meal.entries = m.entries || [];
     MealForm.meal.name = m.name || "Pizza"
     //TODO: auto set current date
-    MealForm.meal.date = m.date || '10/16/2015';
-    console.log('did we get the date alright?', ConvertDate.convert(m.date));
+    MealForm.meal.date = date || '10/16/2015';
     MealForm.meal.location = m.location || 'Tenochtitlan';
     MealForm.meal.rating = m.rating || 5;
     MealForm.meal.notes = m.notes || 'Meal was out of this world';
