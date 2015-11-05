afa372a852b0810a4237c061269eb1aaa3a22397
Scott Schwartz <sschwa12@gmail.com>
2015-11-05 15:04:43 -0600

    View working with dummy data

--- client/controllers/mealPlanner.js
+++ client/controllers/mealPlanner.js
@@ -1,4 +1,64 @@
-app.controller('MealPlanner', ['$scope, $http, $location', function($scope, $http, $location){
+app.controller('MealPlanner', ['$scope', function($scope){
 
-  
+  $scope.days = {
+    "Sunday" : [
+      {
+        name : 'meal1',
+        type : 'breakfast',
+        notes : 'this was a crappy meal',
+        ingredients : [
+          'apples',
+          'butter',
+          'aardvark meat'
+        ]
+      }
+    ],
+
+    "Monday" : [
+      {
+        name : 'meal2',
+        type : 'dinner',
+        notes : 'this was a great meal',
+        ingredients : [
+          'apples',
+          'bread',
+          'rat meat'
+        ]
+      }
+    ],
+
+    "Tuesday" : [
+
+    ],
+
+    "Wednesday" : [
+
+    ],
+
+    "Thursday" : [
+
+    ],
+
+    "Friday" : [
+      {
+        name : 'meal6',
+        type : 'snack',
+        notes : 'this was a stupid meal',
+        ingredients : [
+          'oranges',
+          'butter',
+          'aardvark meat'
+        ]
+      }
+    ],
+
+    "Saturday" : [
+
+    ],
+
+    // "Sunday" : [
+
+    // ]
+  }
+
 }]);
