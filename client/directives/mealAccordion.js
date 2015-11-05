06a887aefbcac89e6af4715f4a1c0e2ef6e1b37d
Diana <dianafaye17@gmail.com>
2015-11-05 10:52:50 -0600

    Hooked up star rating sort on home page. Fixed accordion collapse issue on home page.

--- client/directives/mealAccordion.js
+++ client/directives/mealAccordion.js
@@ -1,10 +1,10 @@
-// mealAccordion directive component for re-use
-app.directive('mealaccordion', function() {
-  return  {
-    // use as attribute or element
-    restrict: 'E',
-    // use sidebar view as template
-    templateUrl: '../views/mealAccordion.html',
-    controller: 'MealView'
-  };
-});
+// // mealAccordion directive component for re-use
+// // not currently being used because it breaks collapsing
+// app.directive('mealaccordion', function() {
+//   return  {
+//     // use as attribute or element
+//     restrict: 'E',
+//     templateUrl: '../views/mealAccordion.html',
+//     controller: 'MealView'
+//   };
+// });
